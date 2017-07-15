e6abac52cf0317751c9e9bef953a4fe33ff9fe63
Paul George <command.paul@gmail.com>
2017-07-15 12:44:09 +0530

    Cleanup

--- riscv-isa-sim/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- int main(void) { 
-	int counter =0;
- 	while(1){
- 		counter ++;
- 		printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Hello world! %d\n",counter);
-		} 	
- 	return 0;
- 	}
